084390f3af03429692e211f824ae727dfd2e6fe2
Kzu <alvaro.vega.pavez@gmail.com>
2023-02-23 15:14:50 -0300

    Commit inicio de proyecto

---
+++ src/main/java/cl/ds/app/mystore/MyStoreApplication.java
@@ -0,0 +1,13 @@
+package cl.ds.app.mystore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyStoreApplication.class, args);
+	}
+
+}
