c4ead9625370b074d3d57213b92f37f175c8a1bf
Poipurin <b.pinorojo@gmail.com>
2023-03-10 08:39:48 -0300

    se añade método añadir/eliminar

--- src/main/java/cl/ds/app/mystore/helper/PlacaMadreHelper.java
+++ src/main/java/cl/ds/app/mystore/helper/PlacaMadreHelper.java
@@ -16,12 +16,40 @@ public class PlacaMadreHelper {
 
     //Método main
     public static void main(String[] args){
-
+    addPlacaMadre(crearPlacaMadre()); //añadir objeto placa madre
+        addPlacaMadre(crearPlacaMadre2());
+        addPlacaMadre(crearPlacaMadre3());//no lo añade porque ya existe
+        System.out.println(placaMadreList);
+    removePlacaMadre(crearPlacaMadre2());
+    System.out.println(placaMadreList);
      }
 
      //Constructor vacío
     private static PlacaMadre crearPlacaMadre(){
         placaMadre = new PlacaMadre();
+        placaMadre.setId("1");
+        placaMadre.setChipset("AMD TRX40");
+        placaMadre.setSocket("sTRX4");
+        placaMadre.setFabricante("Asus");
+        placaMadre.setFormato("EATX");
+        placaMadre.setMemorias("DDR4");
+        return placaMadre;
+    }
+
+    private static PlacaMadre crearPlacaMadre2(){
+        placaMadre = new PlacaMadre();
+        placaMadre.setId("2");
+        placaMadre.setChipset("AMD TRX40");
+        placaMadre.setSocket("sTRX4");
+        placaMadre.setFabricante("Asus");
+        placaMadre.setFormato("EATX");
+        placaMadre.setMemorias("DDR4");
+        return placaMadre;
+    }
+
+    private static PlacaMadre crearPlacaMadre3(){
+        placaMadre = new PlacaMadre();
+        placaMadre.setId("2");
         placaMadre.setChipset("AMD TRX40");
         placaMadre.setSocket("sTRX4");
         placaMadre.setFabricante("Asus");
@@ -32,14 +60,18 @@ public class PlacaMadreHelper {
 
     //Añadir
     private static List<PlacaMadre> addPlacaMadre(PlacaMadre placaMadre){
+        if (!placaMadreList.contains(placaMadre)){//añadir solo si el objeto no existe
         placaMadreList.add(placaMadre);
+        }
         return placaMadreList;
     }
 
     //Eliminar
-    //TODO investigar y aplicar método eliminar por objeto
-
-    //Validaciones
-
+    private static List<PlacaMadre> removePlacaMadre(PlacaMadre placaMadre){
+        if (placaMadreList.contains(placaMadre)){ //si existe se elimina
+            placaMadreList.remove(placaMadre);
+        }
+        return placaMadreList;
+    }
     //existe por ID
 }
