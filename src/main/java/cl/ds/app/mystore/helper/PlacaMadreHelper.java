72ad4d8ab5bf7a3f1c882cfa176bcd96bd5518f6
Poipurin <b.pinorojo@gmail.com>
2023-03-19 04:04:59 -0300

    Se añade método eliminar por Id versión clases

--- src/main/java/cl/ds/app/mystore/helper/PlacaMadreHelper.java
+++ src/main/java/cl/ds/app/mystore/helper/PlacaMadreHelper.java
@@ -15,19 +15,21 @@ public class PlacaMadreHelper {
     private static List<PlacaMadre> placaMadreList = new ArrayList<>();
 
     //Método main
-    public static void main(String[] args){
-    addPlacaMadre(crearPlacaMadre()); //añadir objeto placa madre
+    public static void main(String[] args) {
+        addPlacaMadre(crearPlacaMadre()); //añadir objeto placa madre
         addPlacaMadre(crearPlacaMadre2());
-        addPlacaMadre(crearPlacaMadre3());//no lo añade porque ya existe
+        addPlacaMadre(crearPlacaMadre3());
         System.out.println(placaMadreList);
-    //removePlacaMadre(crearPlacaMadre2());
-    System.out.println(placaMadreList);
-        removeById("1");
+        //removePlacaMadre(crearPlacaMadre2());
+        //System.out.println(placaMadreList);
+        //removeById("1");
         System.out.println(placaMadreList);
-     }
+        eliminarPlacaPorId("1");
+        System.out.println(placaMadreList);
+    }
 
-     //Constructor vacío
-    private static PlacaMadre crearPlacaMadre(){
+    //Constructor vacío
+    private static PlacaMadre crearPlacaMadre() {
         placaMadre = new PlacaMadre();
         placaMadre.setId("1");
         placaMadre.setChipset("AMD TRX40");
@@ -38,7 +40,7 @@ public class PlacaMadreHelper {
         return placaMadre;
     }
 
-    private static PlacaMadre crearPlacaMadre2(){
+    private static PlacaMadre crearPlacaMadre2() {
         placaMadre = new PlacaMadre();
         placaMadre.setId("2");
         placaMadre.setChipset("AMD TRX40");
@@ -49,9 +51,9 @@ public class PlacaMadreHelper {
         return placaMadre;
     }
 
-    private static PlacaMadre crearPlacaMadre3(){
+    private static PlacaMadre crearPlacaMadre3() {
         placaMadre = new PlacaMadre();
-        placaMadre.setId("2");
+        placaMadre.setId("3");
         placaMadre.setChipset("AMD TRX40");
         placaMadre.setSocket("sTRX4");
         placaMadre.setFabricante("Asus");
@@ -61,40 +63,22 @@ public class PlacaMadreHelper {
     }
 
     //Añadir
-    private static List<PlacaMadre> addPlacaMadre(PlacaMadre placaMadre){
-        if (!placaMadreList.contains(placaMadre)){//añadir solo si el objeto no existe
-        placaMadreList.add(placaMadre);
+    private static List<PlacaMadre> addPlacaMadre(PlacaMadre placaMadre) {
+        if (!placaMadreList.contains(placaMadre)) {//añadir solo si el objeto no existe
+            placaMadreList.add(placaMadre);
         }
         return placaMadreList;
     }
 
     //Eliminar
-    private static List<PlacaMadre> removePlacaMadre(PlacaMadre placaMadre){
-        if (placaMadreList.contains(placaMadre)){ //si existe se elimina
+    private static List<PlacaMadre> removePlacaMadre(PlacaMadre placaMadre) {
+        if (placaMadreList.contains(placaMadre)) { //si existe se elimina
             placaMadreList.remove(placaMadre);
         }
         return placaMadreList;
     }
-    //existe por ID
 
-    private static boolean existeId(String id) {
-        boolean esVlido = false;
-        System.out.println("existe??? " + placaMadre.getId());
-        System.out.println("existe??? " + placaMadre.getId().equals(id));
-
-        System.out.println("placa.getId() " + placaMadre.getId());
-        for (PlacaMadre placa: placaMadreList) {
-            System.out.println("placa " + placa);
-            if (placa.getId().equals(id)) {
-                System.out.println("lo encontre!!");
-
-                esVlido = true;
-                break;
-            }
-        }
-        return esVlido;
-    }
-
+    //Eliminar por ID
     private static void removeById(String id) {
         Optional placaMadreOpt = placaMadreList.stream()
                 .filter(placaMadre -> placaMadre.getId().equals(id))
@@ -104,10 +88,35 @@ public class PlacaMadreHelper {
             System.out.println("largo lista " + placaMadreList.size());
             System.out.println("La placa con ID " + id + " fue eliminada");
         } else {
-            System.out.println("No se encuentra la placa madre, intente con otro ID");
+            System.out.println("No se encuentra la placa madre con el iD ingresado");
         }
     }
-
-
+    //Existe por ID
+    private static boolean existeId(String id) {
+        boolean esVlido = false;
+        System.out.println("placaMadre.getId() " + placaMadre.getId());
+        for (PlacaMadre placa : placaMadreList) {
+            if (placa.getId().equals(id)) {
+                esVlido = true;
+                break;
+            }
+        }
+        return esVlido;
     }
+    //Eliminar por Id versión 2
+    private static void eliminarPlacaPorId(String id) {
+        boolean existeId = existeId(id);
+        if (existeId) {
+            for (PlacaMadre placa : placaMadreList) {
+                if (placa.getId().equals(id)) {
+                    placaMadreList.remove(placa);
+                    System.out.println("La placa con ID " + id + " fue eliminada");
+                    break;
+                } else {
+                    System.out.println("No se encuentra la placa madre con el ID ingresado");
+                }
+            }
+        }
+    }
+}
 
